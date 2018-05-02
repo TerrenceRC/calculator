9ee0acd35460d6d192497f9d883ebafe5a451f68
Terrence Cardwell <terrencerc@gmail.com>
2018-05-02 04:40:30 -0500

    Initial commit, operator functions created

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition(numA, numB) {
+	return numA + numB;
+}
+
+function subtraction(numA, numB) {
+	return numA - numB;
+}
+
+function divide(numA, numB) {
+	return numA / numB;
+}
+
+function multiply(numA, numB) {
+	return numA * numB;
+}
+
+function operate(operator, numA, numB) {
+	return operator(numA, numB);
+}
